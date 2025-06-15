838aaa565603f91918853ed284a3a8c1fd80e4e3
datngo2001 <ngominhdat110115@gmail.com>
2025-06-15 19:57:23 +0700

    add detail page

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -9,6 +9,7 @@ module('Integration | Component | rental', function (hooks) {
   test('it renders information about a rental property', async function (assert) {
     this.setProperties({
       rental: {
+        id: 'grand-old-mansion',
         title: 'Grand Old Mansion',
         owner: 'Veruca Salt',
         city: 'San Francisco',
@@ -27,13 +28,17 @@ module('Integration | Component | rental', function (hooks) {
     });
 
     await render(hbs`<Rental @rental={{this.rental}} />`);
-    
+
     assert.dom('article').hasClass('rental');
     assert.dom('article h3').hasText('Grand Old Mansion');
+    assert
+      .dom('article h3 a')
+      .hasAttribute('href', '/rentals/grand-old-mansion');
     assert.dom('article .detail.owner').includesText('Veruca Salt');
     assert.dom('article .detail.type').includesText('Standalone');
     assert.dom('article .detail.location').includesText('San Francisco');
     assert.dom('article .detail.bedrooms').includesText('15');
     assert.dom('article .image').exists();
+    assert.dom('article .map').exists();
   });
 });
