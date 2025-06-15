31982d0dedca70d07de5341e04ab8277dadd01bb
datngo2001 <ngominhdat110115@gmail.com>
2025-06-15 22:48:31 +0700

    add search

--- tests/unit/models/rental-test.js
+++ tests/unit/models/rental-test.js
@@ -5,6 +5,7 @@ module('Unit | Model | rental', function (hooks) {
   setupTest(hooks);
 
   test('it has the right type', function (assert) {    
+    const store = this.owner.lookup('service:store');
     let rental = store.createRecord('rental', {
       id: 'grand-old-mansion',
       title: 'Grand Old Mansion',
